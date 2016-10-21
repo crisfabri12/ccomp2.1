e0d0ecc16c9880cba3f14d1ef7db9ae9d6a35497
crisfabri12 <cristhian.ocola@ucsp.edu.pe>
2016-10-21 10:11:57 -0500

    Update pointarray.cpp

--- pointarray.cpp
+++ pointarray.cpp
@@ -3,9 +3,27 @@ using namespace std;
 #include "pointarray.h"
 pointarray::pointarray(const point points[],const int siz){
 sizze = siz;
-p = point points[];
+*p =points[siz];
+int i=0;
+for (i=0;i < siz;++i)
+    p[i] = points[i];
 }
 int pointarray::getSize(){
 return sizze;
 }
+void pointarray::point_f(const point &x){
+    int i;
+    for (i=0;i<sizze;++i)
+        ;
+    p[i] = x;
+}
+void pointarray::inser(const int position, const point &x){
+    int i;
+    for(i=0;i<position;++i)
+        ;
+    p[i] = x;
+
+}
+
+
 
