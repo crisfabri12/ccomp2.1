7efeb0fc62fb1ea1730be28eccf1e03a6f8c0449
crisfabri12 <cristhian.ocola@ucsp.edu.pe>
2016-10-26 20:22:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include "point.h"
 #include "vector.h"
+#include "pointarray.h"
 using namespace std;
 main()
 {
 point r(1,2);
 point t(3,4);
-vec x(r,t);
-x.print();
+point y(5,4);
+point p[] ={r,t};
+point x[] ={r,y};
+pointarray f(p,2);
+pointarray v(f);
+v.inser(2,y);
+v.remov(1);
+point h;
+v.clearr();
+h = v.p[1];
+
+
+
+
 
 }
